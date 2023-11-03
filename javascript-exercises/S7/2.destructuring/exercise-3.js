4fca96b78c4176d83f8de5a381f89da9e8056bf9
Darioooo <darionietosastre@gmail.com>
2023-11-03 12:00:03 +0100

    Se añaden ejercicios s7-1 s7-2 y s7-3

---
+++ javascript-exercises/S7/2.destructuring/exercise-3.js
@@ -0,0 +1,9 @@
+/* En base al siguiente javascript, usa destructuring para crear 2 variables igualandolo a la función e imprimiendolo por consola. */
+
+const animalFunction = () => {
+    return {name: 'Bengal Tiger', race: 'Tiger'}
+};
+
+const {name, race} = animalFunction();
+console.log('nombre:',name);
+console.log('raza:',race);
